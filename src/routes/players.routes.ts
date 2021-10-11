d44727c6822166995b1ace1ca54a6e5d9280a283
Yusuf Ali <yali98319@gmail.com>
2021-10-11 10:59:35 +0530

    update routes

--- src/routes/players.routes.ts
+++ src/routes/players.routes.ts
@@ -3,28 +3,30 @@ import { FastifyInstance, RouteOptions } from 'fastify';
 import PlayerControllers from '../controllers/players.controllers';
 import PlayerSchemas from '../schemas/players.schemas';
 
-type TAppRoutesProvider = (
+type TProvider = (
   fastify: FastifyInstance,
   options: RouteOptions,
   done: Function
 ) => void;
 
-const appRoutesProvider: TAppRoutesProvider = (fastify, options, done) => {
-  fastify.route({
+const allRoutes: RouteOptions[] = [
+  {
     method: 'GET',
     url: '/players',
     schema: PlayerSchemas.GetPlayers,
     handler: PlayerControllers.getPlayers,
-  });
-
-  fastify.route({
+  },
+  {
     method: 'POST',
     url: '/player',
     schema: PlayerSchemas.AddPlayer,
     handler: PlayerControllers.addPlayer,
-  });
+  },
+];
 
+const PlayerRoutesProvider: TProvider = (fastify, _, done) => {
+  allRoutes.forEach((r) => fastify.route(r));
   done();
 };
 
-export default appRoutesProvider;
+export default PlayerRoutesProvider;
