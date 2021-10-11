1e387d2e32d518819fd38bd9bf313b8f20dee6de
Yusuf Ali <yali98319@gmail.com>
2021-10-11 10:59:49 +0530

    integrate swagger for api docs

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import fastify from 'fastify';
 import fastifyPostgres from 'fastify-postgres';
+import fastifySwagger from 'fastify-swagger';
 
 import envVars from './envConfig';
-import playersRoutes from './routes/players.routes';
+import PlayerRoutesProvider from './routes/players.routes';
 
 const fastifyServer = fastify({ logger: true });
 const { PORT } = envVars;
-
-fastifyServer.register(playersRoutes);
-
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 fastifyServer.register(fastifyPostgres, { connectionString });
+fastifyServer.register(fastifySwagger, { exposeRoute: true });
+fastifyServer.register(PlayerRoutesProvider);
 
 const start = async () => {
   try {
